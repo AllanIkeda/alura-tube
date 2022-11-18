ae8c5625980ae185d25189a2964630669318f2f3
Allan Ikeda <allanikeda@outlook.com>
2022-11-18 17:27:49 -0300

    aula-04

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import ColorModeProvider, { ColorModeContext } from "../src/components/Menu/componentes/ColorMode";
+import RegisterVideo from "../src/components/Menu/componentes/RegisterVideo";
+
 
 const theme = {
   light: {
@@ -28,13 +30,14 @@ function ProviderWrapper(props) {
   )
 }
 
-function MyApp({ Component, pageProps }) {
+function Root({ Component, pageProps }) {
   const contexto = React.useContext(ColorModeContext);
 
   return (
     <ThemeProvider theme={theme[contexto.mode]}>
       <CSSReset />
       <Component {...pageProps} />
+      <RegisterVideo />
     </ThemeProvider>
   )
 }
@@ -42,7 +45,7 @@ function MyApp({ Component, pageProps }) {
 export default function _App(props) {
   return (
     <ProviderWrapper>
-      <MyApp {...props} />
+      <Root {...props} />
     </ProviderWrapper>
   )
 };
